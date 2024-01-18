ee65fea49b2df2ed65a413013540c87f5bcfff46
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-01-18 15:16:38 +0300

    Initial Commit

---
+++ PushNotificationTutorial/PushNotificationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationTutorialApp.swift
+//  PushNotificationTutorial
+//
+//  Created by Fatih Durmaz on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
