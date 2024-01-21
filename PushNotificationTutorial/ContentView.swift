f1d2eace5c604421a89a122e82ff3bf1c753d746
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-01-22 02:49:45 +0300

    first commit

--- PushNotificationTutorial/ContentView.swift
+++ PushNotificationTutorial/ContentView.swift
@@ -6,16 +6,64 @@
 //
 
 import SwiftUI
+import Lottie
+import StepperView
 
 struct ContentView: View {
+    let steps = [ Text("DeviceToken Üretmek").font(.title3),
+                  Text("Kullanıcı İzni Almak").font(.title3),
+                  Text("Xcode Push Notifications Capabilities").font(.title3),
+                  Text("Sertifika Üretmek").font(.title3),
+                  Text("Push Notifications Console ile Bildirim Göndermek").font(.title3)
+    ]
+    
+    let indicationTypes: [StepperIndicationType] = [
+        .custom(NumberedCircleView(text: "1", color: .black)),
+        .custom(NumberedCircleView(text: "2", color: .black)),
+        .custom(NumberedCircleView(text: "3", color: .black)),
+        .custom(NumberedCircleView(text: "4", color: .black)),
+        .custom(NumberedCircleView(text: "5", color: .black))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                LottieView(animation: .named("push"))
+                    .looping()
+                Divider()
+                StepperView()
+                    .addSteps(steps)
+                    .indicators(indicationTypes)
+                    .stepIndicatorMode(StepperMode.vertical)
+                    .lineOptions(StepperLineOptions.custom(1, Colors.black.rawValue))
+                
+                    .spacing(30)
+                    .italic()
+            }
+            .onAppear{
+                
+                Task {
+                    let center = UNUserNotificationCenter.current()
+                    
+                    do {
+                        let success = try await center.requestAuthorization(options: [.alert, .badge, .sound])
+                        
+                        if success {
+                            UIApplication.shared.registerForRemoteNotifications()
+                            print("Push notification allowed by user")
+                        } else {
+                            print("Push notification not allowed by user")
+                        }
+                        
+                    } catch {
+                        print("Error")
+                    }
+                }
+
+            }
+            .padding()
+            .navigationTitle("Push Notifications")
         }
-        .padding()
     }
 }
 
--- PushNotificationTutorial/PushNotificationTutorialApp.swift
+++ PushNotificationTutorial/PushNotificationTutorialApp.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct PushNotificationTutorialApp: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
+    init(){
+        let appareance = UINavigationBarAppearance()
+        appareance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = appareance
+        UINavigationBar.appearance().scrollEdgeAppearance = appareance
+    }
+        
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data ) {
+        let token = deviceToken.map {String(format: "%02.2hhx", $0)}.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+    }
+    
 }
